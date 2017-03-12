274571251c4118a63a51e39b6eb5629705406123
evavegh <eva.vegh.93@gmail.com>
2017-03-12 14:17:19 +0100

    Events site added

--- src/main/java/hu/evave/eventfinder/web/web/EventsController.java
+++ src/main/java/hu/evave/eventfinder/web/web/EventsController.java
@@ -1,14 +1,24 @@
 package hu.evave.eventfinder.web.web;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hu.evave.eventfinder.web.repository.EventRepository;
 
 @Controller
 public class EventsController {
 
-	@RequestMapping("/")
-	public String home() {
-		return "index";
+	@Autowired
+	EventRepository eventRepository;
+
+	@RequestMapping("/events")
+	public String home(Map<String, Object> model) {
+		model.put("events", eventRepository.findAll());
+		return "/events";
+
 	}
 
 }
