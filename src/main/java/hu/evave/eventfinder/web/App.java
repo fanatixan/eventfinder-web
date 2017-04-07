2b0b64ba81a29364a4ff2601be28accd080b08b0
Imre Gábor <gabor@aut.bme.hu>
2017-04-07 10:45:47 +0200

    turn on default security

--- src/main/java/hu/evave/eventfinder/web/App.java
+++ src/main/java/hu/evave/eventfinder/web/App.java
@@ -2,8 +2,11 @@ package hu.evave.eventfinder.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class App 
 {
     public static void main( String[] args )
