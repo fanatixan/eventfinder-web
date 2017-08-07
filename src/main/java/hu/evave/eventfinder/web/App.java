d67e8a25c5bd45c62bfcb54f7825c26100168c61
fanatixan <attila@javadev.hu>
2017-08-07 18:52:06 +0200

    run in local

--- src/main/java/hu/evave/eventfinder/web/App.java
+++ src/main/java/hu/evave/eventfinder/web/App.java
@@ -2,14 +2,12 @@ package hu.evave.eventfinder.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
 }
--- src/main/java/hu/evave/eventfinder/web/web/EventsController.java
+++ src/main/java/hu/evave/eventfinder/web/web/EventsController.java
@@ -28,95 +28,96 @@ import hu.evave.eventfinder.web.repository.UserRepository;
 @Controller
 public class EventsController {
 
-	@Autowired
-	EventRepository eventRepository;
-	
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    EventRepository eventRepository;
 
-	@RequestMapping("/events")
-	public String listEvents(Map<String, Object> model) {
-		model.put("events", eventRepository.findAll());
-		return "events";
+    @Autowired
+    UserRepository userRepository;
 
-	}
-	
-	@RequestMapping(value = "/events/json", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	public String listEventsJson() {
-		return new Gson().toJson(eventRepository.findAll());
-	}
-	
-	@RequestMapping(value = "/myevents/json", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
-	public String listMyEventsJson() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userRepository.findByName(auth.getName());
-		return new Gson().toJson(eventRepository.findByCreatedBy(user));
-	}
-	
-	@RequestMapping("/myevents")
-	public String listMyEvents(Map<String, Object> model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userRepository.findByName(auth.getName());
-		model.put("events", eventRepository.findByCreatedBy(user));
-		return "myevents";
+    @RequestMapping("/events")
+    public String listEvents(Map<String, Object> model) {
+        model.put("events", eventRepository.findAll());
+        return "events";
 
-	}
+    }
 
-	@RequestMapping(value = "/delete", method = RequestMethod.POST)
-	public @ResponseBody List<Event> delete(@RequestBody Event eventToDelete) {
-				
-		//valójában nem töröljük ki az adatbázisból
-		eventToDelete.setStartsAt(null);
-		eventRepository.save(eventToDelete);
-		return eventRepository.findAll();
+    @RequestMapping(value = "/events/json", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String listEventsJson() {
+        return new Gson().toJson(eventRepository.findAll());
+    }
 
-	}
-	
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String add(Map<String, Object> model) {	
-		
-		Event event = new Event();
-		event.setId(-1L);
-		event.setLocation(new Location());
-		event.setTypes(new ArrayList<>());
-		model.put("event", event);
-		model.put("allTypes", EventType.values());
-		model.put("currencies", Currency.values());
-		return "edit";
-	}
-	
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String add(@ModelAttribute("event") Event event) {
+    @RequestMapping(value = "/myevents/json", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String listMyEventsJson() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByName(auth.getName());
+        return new Gson().toJson(eventRepository.findByCreatedBy(user));
+    }
 
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		event.setCreatedBy(userRepository.findByName(auth.getName()));
-		
-		if(event.getId() != null && event.getId() < 0) {
-			event.setId(null);
-		}
-		System.out.println(event);
-		eventRepository.save(event);
-		return "redirect:/myevents";
+    @RequestMapping("/myevents")
+    public String listMyEvents(Map<String, Object> model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByName(auth.getName());
+        model.put("events", eventRepository.findByCreatedBy(user));
+        return "myevents";
 
-	}
-	
-	@RequestMapping(value = "/edit/{eventId}", method = RequestMethod.GET)
-	public String edit(@PathVariable("eventId") long id, Map<String, Object> model) {	
-		
-		model.put("event", eventRepository.findOne(id));
-		model.put("allTypes", EventType.values());
-		model.put("currencies", Currency.values());
-		return "edit";
+    }
 
-	}
-	
-	@RequestMapping(value = "/edit/{eventId}", method = RequestMethod.POST)
-	public String edit(@PathVariable("eventId") long id, @ModelAttribute("event") Event event) {
-		eventRepository.save(event);
-		return "redirect:/myevents";
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public @ResponseBody List<Event> delete(@RequestBody Event eventToDelete) {
 
-	}
+        // valójában nem töröljük ki az adatbázisból
+        Event event = eventRepository.findOne(eventToDelete.getId());
+        event.setStartsAt(null);
+        eventRepository.save(event);
+        return eventRepository.findAll();
+
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(Map<String, Object> model) {
+
+        Event event = new Event();
+        event.setId(-1L);
+        event.setLocation(new Location());
+        event.setTypes(new ArrayList<>());
+        model.put("event", event);
+        model.put("allTypes", EventType.values());
+        model.put("currencies", Currency.values());
+        return "edit";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(@ModelAttribute("event") Event event) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        event.setCreatedBy(userRepository.findByName(auth.getName()));
+
+        if (event.getId() != null && event.getId() < 0) {
+            event.setId(null);
+        }
+        System.out.println(event);
+        eventRepository.save(event);
+        return "redirect:/myevents";
+
+    }
+
+    @RequestMapping(value = "/edit/{eventId}", method = RequestMethod.GET)
+    public String edit(@PathVariable("eventId") long id, Map<String, Object> model) {
+
+        model.put("event", eventRepository.findOne(id));
+        model.put("allTypes", EventType.values());
+        model.put("currencies", Currency.values());
+        return "edit";
+
+    }
+
+    @RequestMapping(value = "/edit/{eventId}", method = RequestMethod.POST)
+    public String edit(@PathVariable("eventId") long id, @ModelAttribute("event") Event event) {
+        eventRepository.save(event);
+        return "redirect:/myevents";
+
+    }
 
 }
